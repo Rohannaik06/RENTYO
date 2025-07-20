55da6cf6a56d6062aa2d00c7835e426164ad1bc1
Rohan Naik <rohannaik29563@gmail.com>
2025-07-20 21:05:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ function showRegisterForm() {
+      document.getElementById("register-form").style.display = "block";
+      document.getElementById("login-form").style.display = "none";
+    }
+
+    function showLoginForm() {
+      document.getElementById("login-form").style.display = "block";
+      document.getElementById("register-form").style.display = "none";
+    }
